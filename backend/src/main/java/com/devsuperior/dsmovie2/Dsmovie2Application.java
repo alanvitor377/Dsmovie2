1a92498837f97ed62d621f8a21e64825c5734516
Alan Vitor <alanvitor377@gmail.com>
2022-05-15 03:57:31 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/dsmovie2/Dsmovie2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmovie2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmovie2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmovie2/Dsmovie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmovie2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
